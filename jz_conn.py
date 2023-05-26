858dbb16a7a985a158f0364831fd676b84cf27c0
Serigne Ousmane Kana Ciss <kanaman6@gmail.com>
2023-05-26 09:57:34 -0500

    Update
    Add of the new logic to generate offer letters

--- jz_conn.py
+++ jz_conn.py
@@ -47,15 +47,7 @@ def get_records():
         "Birthday"
       
         ]
-    # new_header = records_df.iloc[0]
-    # records_df = records_df[1:]
-
-    # print(records_df.head())
-
-    # records_df.to_csv("dumps.csv")
-
-    # records_df["SSN"] = records_df["SSN"].apply(lambda x: str(x)[-5:])
-
+    
     
     id_grand = {}
 
