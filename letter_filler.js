e397f5a4cdd61873024d111180c118b978004543
Serigne Ousmane Kana Ciss <kanaman6@gmail.com>
2023-05-26 09:44:47 -0500

    Update

--- letter_filler.js
+++ letter_filler.js
@@ -3,7 +3,7 @@ this.getField("full_name").value = "$first_name"
 this.getField("student_name1").value = "$first_name"
 this.getField("student_id").value = 10004321
 this.getField("letter_subject").value = "Letter Subject"
-this.getField("address_street").value = "11929 W Airport Blvd"
+this.getField("address_street").value = "11929 W Airport Blvvvvvvd"
 this.getField("address_city").value = "Stafford"
 this.getField("address_state").value = "Texas"
 
--- test1.py
+++ test1.py
@@ -3,10 +3,9 @@ from PyPDF2 import PdfWriter, PdfReader
 output = PdfWriter()
 ipdf = PdfReader(open('template.pdf', 'rb'))
 
-for i in range(len(ipdf.pages)):
-    page = ipdf.pages[i]
-    output.add_page(page)
+page = ipdf.pages[0]
+output.add_page(page)
 
 with open('new.pdf', 'wb') as f:
-    output.add_js("alert('Fak you')")
+    output.add_js('this.getField("student_name1").value = "$first_name"')
     output.write(f)
